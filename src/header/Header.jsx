346c35b040ecfa21cae7704acb2a024587aa6f00
Gage Broberg <gagebroberg@gmail.com>
2022-11-25 12:16:45 -0600

    Adding initial header and top nav

---
+++ src/header/Header.jsx
@@ -0,0 +1,13 @@
+import "./header.css"
+
+export default function Header() {
+  return (
+    <div className="header">
+        <div className="headerTitles">
+            <span className="headerTitleSm">React & Node</span>
+            <span className="headerTitleLg">Blog</span>
+        </div>
+        <img className="headerImg" src="https://media.istockphoto.com/id/1353553203/photo/forest-wooden-table-background-summer-sunny-meadow-with-green-grass-forest-trees-background.jpg?b=1&s=170667a&w=0&k=20&c=-jvR1WDwcloLXRgRTGeyG3frvrhPIbegdemeL6vY2Pk=" alt="" />
+    </div>
+  )
+}
